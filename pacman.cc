a10f411e9477147cfb80c175e8e363cc1e833b2d
Asmaou BAH <bahasma90@gmail.com>
2026-01-15 20:59:57 +0100

    Add files via upload

--- pacman.cc
+++ pacman.cc
@@ -15,11 +15,33 @@ std::string position::tostring() const{
     return "(" + std::to_string(_x)  +"," + std::to_string(_y) + ")";
 }
 
+//appliquer le deplacement sur une position selon la direction entrer en parametre
+void position::appliquerdeplacement(direction dir){
+    switch (dir) {
+    case direction::droite:
+        ++_x;
+        break;
+    case direction::gauche:
+        --_x;
+        break;
+    case direction::bas:
+        ++_y;
+        break;
+    case direction::haut:
+        --_y;
+        break;
+    case direction::stop:
+        break;
+    default:
+        break;
+    }
+}
+
 std::ostream & operator<<(std::ostream & os, position const & p) {
     return os << p.tostring();
 }
 
-taille::taille(coord hauteur, coord largeur)
+taille::taille(coord largeur, coord hauteur)
     :_largeur(largeur), _hauteur(hauteur){}
 
 coord taille::h() const{
@@ -82,8 +104,8 @@ void enmouvement::sortie_flux(std::ostream & os) const{
     }
 }
 
-pacman::pacman(position const & p,direction dir)
-    :enmouvement(p, dir), _niveau(0){}
+pacman::pacman(position const & p)
+    :enmouvement(p, direction::stop), _niveau(0){}
 
 bool pacman::invincible() const{
     return _niveau > 0;
@@ -106,12 +128,21 @@ void pacman::sortie_flux(std::ostream & os) const{
     os << " niveau d'invincibilité : " << _niveau;
 }
 
-fantome::fantome(position const & p, direction dir)
-    :enmouvement(p, dir){}
+std::unique_ptr<element> pacman::clone() const{
+    return std::make_unique<pacman>(*this);
+}
+
+
+fantome::fantome(position const & p)
+    :enmouvement(p, direction::stop){}
 
 taille fantome::tai() const{
     return taille(20,20);
 }
+std::unique_ptr<element> fantome::clone() const{
+    return std::make_unique<fantome>(*this);
+}
+
 
 pacgomme::pacgomme(position const & p)
     :element(p){}
@@ -119,10 +150,13 @@ pacgomme::pacgomme(position const & p)
 taille pacgomme::tai() const{
     return taille(3,3);
 }
+std::unique_ptr<element> pacgomme::clone() const{
+    return std::make_unique<pacgomme>(*this);
+}
 
 mur::mur(position const & p, taille const & t)
     :element(p),
-    _tai(t.h() >= 10 && t.w() >= 10 ? t : taille(10, 10)){
+    _tai(t){
 
 }
 
@@ -130,3 +164,258 @@ taille mur::tai() const{
     return _tai;
 }
 
+std::unique_ptr<element> mur::clone() const{
+    return std::make_unique<mur>(*this);
+}
+
+std::unique_ptr<mur> mur::fabrique(position const & p, taille const & t){
+    if(t.w() >= 10 && t.h() >= 10){
+        return std::unique_ptr<mur>(new mur(p, t));
+    }
+    else{
+        return std::unique_ptr<mur>();
+    }
+}
+
+bool element::contient(element const & o) const {
+    return (o.pos().x()>=pos().x()) && (o.pos().y()>=pos().y()) && ((o.pos().x()+o.tai().w()) <= pos().x()+tai().w()) && ((o.pos().y()+o.tai().h()) <= pos().y()+tai().h());
+}
+
+bool intersectionintervalle(unsigned int a1, unsigned int a2, unsigned int b1, unsigned int b2) {
+    return b1 < a2 && a1 < b2;
+}
+bool element::intersection(const element &o) const {
+    return intersectionintervalle(pos().x(), pos().x()+tai().w(), o.pos().x(), o.pos().x()+o.tai().w()) && intersectionintervalle(pos().y(), pos().y()+tai().h(), o.pos().y(), o.pos().y()+o.tai().h());
+}
+
+
+jeu::jeu(jeu const & j)
+    :_etat(j._etat){
+    for(auto const & i : j._elements){
+        _elements.push_back(i->clone());
+    }
+}
+
+jeu & jeu::operator=(jeu const & j) {
+    if(this != &j){
+        _etat = j._etat;
+        _elements.clear();
+        for(auto const & e : j._elements){
+            _elements.push_back(e->clone());
+        }
+    }
+    return *this;
+}
+
+void jeu::afficher(std::ostream & os) const{
+    os << "Etat du jeu : ";
+    switch(_etat){
+    case type::encours:
+        os << " encours";
+        break;
+    case type::defaite:
+        os << "defaite";
+        break;
+    case type::victoire:
+        os << "victoire";
+        break;
+    default :
+        break;
+    }
+
+    os << " ";
+    for(auto const & e : _elements){
+        e->sortie_flux(os);
+        os << "\n";
+    }
+}
+
+char const* exceptionjeu::what() const noexcept{
+    return _nature.c_str();
+}
+
+//ajout d'un element dans un
+void jeu::ajouter(std::unique_ptr<element> e) {
+   auto f = std::find_if(
+               _elements.begin(),
+               _elements.end(),
+               [&e](auto const & i) {
+       return i->intersection(*e);
+   });
+   if(f == _elements.end()){
+        _elements.push_back(std::move(e));
+   }
+   else{
+        throw exceptionjeu("Collision : impossible d'ajouter ici");
+   }
+}
+
+//ajout un nombre e de fantomes au jeu à des positions aleatoires
+void jeu::ajouterfantomes(int nbf) {
+    while(nbf != 0){
+        try {
+           ajouter(std::make_unique<fantome>(position(static_cast<unsigned int>(rand()%320), static_cast<unsigned int>(rand()%200))));
+            --nbf;
+        } catch (exceptionjeu const &) {
+        }
+    }
+}
+
+//ajout un nombre e de fantomes au jeu à des positions aleatoires
+void jeu::ajouterpacgommes(int nbg) {
+    while(nbg != 0){
+        try {
+           ajouter(std::make_unique<pacgomme>(position(static_cast<unsigned int>(rand()%320), static_cast<unsigned int>(rand()%200))));
+           --nbg;
+        } catch (exceptionjeu const &) {
+        }
+    }
+}
+
+//recherche du pacaman du jeu
+const pacman & jeu::accespacman() const{
+    auto f = std::find_if(
+       _elements.begin(),
+        _elements.end(),
+            [](auto const & e){
+        return (dynamic_cast<pacman const *> (e.get()) != nullptr);
+    });
+    if(f != _elements.end()){
+       return *dynamic_cast<pacman const *>(f->get());
+    }
+    else{
+        throw exceptionjeu("Pacgman introuvable");
+    }
+
+}
+
+//changer la direction du pacman si trouver dans le plateau de jeu
+void jeu::directionjoueur(direction dir) {
+    auto f = std::find_if(
+       _elements.begin(),
+        _elements.end(),
+            [](auto const & e){
+        return (dynamic_cast<pacman const *> (e.get()) != nullptr);
+    });
+
+    if(f == _elements.end()){
+        throw exceptionjeu("Pacman introuvable");
+    }
+    else{
+        dynamic_cast<pacman *>(f->get())->setdir(dir);
+    }
+}
+
+//deplacement des elements actifs dans la direction choisie s'il ya collision avec un mur
+void jeu::appliquerdeplacementcollisionmur(enmouvement & em){
+    if(em.deplacement() != direction::stop){
+        auto ancienpos(em.pos()), newpos(ancienpos);
+        newpos.appliquerdeplacement(em.deplacement());
+        em.setpos(newpos);
+        auto f = std::find_if(
+                    _elements.begin(),
+                    _elements.end(),
+                    [&em](std::unique_ptr<element> const & i) {
+            return (dynamic_cast<mur*>(i.get()) != nullptr) && i->intersection(em);
+
+        });
+        if(f != _elements.end()){
+            em.setpos(ancienpos); //pas de collision
+        }
+    }
+}
+
+//verifie si le pacman n'entre pas en collision avec un fantome
+void jeu::appliquerdeplacementcontact(pacman & pm){
+    //s'il est invincible,
+    if(!pm.invincible()){
+        auto f = std::find_if(
+                    _elements.begin(),
+                    _elements.end(),
+                    [&pm](auto const & i){
+            return dynamic_cast<fantome *>(i.get()) != nullptr && i->intersection(pm);
+        });
+        // on a collision avec un fantome,game over
+        if(f != _elements.end()){
+            _etat = type::defaite;
+        }
+    }
+    //pacman invincible
+    else{
+        //on supprime le fantome s'il y a collision
+        _elements.erase(
+                    std::remove_if(
+                        _elements.begin(),
+                        _elements.end(),
+                        [&pm](auto const & i){
+            return (dynamic_cast<fantome *>(i.get()) != nullptr) && i->intersection(pm);
+
+        }), _elements.end());
+    }
+
+}
+
+//pacman mange les pacgommes si ils sont dans le meme rectangle
+void jeu::appliquerdeplacementmanger(pacman & pm){
+    auto it(_elements.begin());
+    while(it != _elements.end()){
+        //s'ils sont en collisions
+        if((dynamic_cast<pacgomme const *>(it->get()) != nullptr) && pm.contient(**it)){
+            it = _elements.erase(it); //le pacgomme disparaait
+            pm.devenirinvincible(); //le pacman gagne en invincibilité
+        }
+        else{
+            ++it;
+        }
+    }
+}
+
+//fonction qui permet de choisir au hasard une direction
+direction randomdirection(){
+    switch(rand() % 4){
+    case 0 : return direction::haut;
+    case 1: return direction::bas;
+    case 2: return direction::droite;
+    case 3: return direction::gauche;
+    }
+    return direction::stop;
+}
+
+//changer la direction d'un fantome
+void fantome::changerdirection() {
+    setdir(randomdirection());
+}
+//changer aleatoirement la direction des fantomes
+void jeu::changerdirectionfantomes(){
+    for(auto & e : _elements){
+        auto f(dynamic_cast<fantome *>(e.get()));
+
+        if(f) {
+            f->changerdirection();
+        }
+    }
+}
+
+//tour d'un tour de jeu (victoire quand y a plus de pacgommes)
+void jeu::tourdejeu() {
+    //appliquer les deplacements pour les elements actifs
+    for(auto & e : _elements){
+        auto em(dynamic_cast<enmouvement *>(e.get()));
+        if(em){
+            appliquerdeplacementcollisionmur(*em);
+        }
+    }
+    for(auto & e : _elements){
+        auto pm(dynamic_cast<pacman *>(e.get()));
+        if(pm){
+            appliquerdeplacementcontact(*pm);
+            appliquerdeplacementmanger(*pm);
+            pm->decrementerinvincible();
+        }
+    }
+
+    //verifier s'il reste des pacmans
+    if(std::none_of(_elements.begin(), _elements.end(), [](auto const & e){return dynamic_cast<pacgomme const *>(e.get()) != nullptr;})) {
+        _etat = type::victoire;
+    }
+}
--- pacman.hh
+++ pacman.hh
@@ -1,5 +1,10 @@
 #pragma once
 #include<iostream>
+#include<list>
+#include<memory>
+#include<exception>
+#include<algorithm>
+#include<random>
 
 enum class direction{
     stop, droite, gauche, haut, bas
@@ -19,6 +24,8 @@ public:
     bool operator!=(position const & p) const {
         return !operator==(p);
     };
+
+    void appliquerdeplacement(direction dir);
 
 private:
     coord _x;
@@ -57,25 +64,33 @@ public:
     void setpos(position const & p){
         _pos = p;
     };
-
+    virtual void sortie_flux(std::ostream & os) const;
     virtual char typeobjet() const = 0;
+    bool contient(element const & e) const;
+    bool intersection(element const & e) const;
+    virtual direction deplacement() const{
+        return direction::stop;
+    }
+    virtual std::unique_ptr<element> clone() const = 0;
+
 private:
     position _pos;
 
 };
 
-
+std::ostream & operator<<(std::ostream & os, element const & e);
 
 //les elments pouvant se deplacer
 class enmouvement : public element{
 public:
-    enmouvement(position const & p, direction dir);
-    direction deplacement() const{
+    enmouvement(position const & p, direction);
+    direction deplacement() const override{
         return _dir;
     };
     void setdir(direction dir){
         _dir = dir;
     }
+    void sortie_flux(std::ostream & os) const override;
 private:
     direction _dir;
 };
@@ -85,7 +100,7 @@ private:
 //les pacman
 class pacman : public enmouvement{
 public:
-    pacman(position const & p, direction dir);
+    pacman(position const & p);
     bool invincible() const;
     void decrementerinvincible();
     void devenirinvincible();
@@ -93,26 +108,99 @@ public:
     char typeobjet() const override{
         return 'P';
     };
+    void sortie_flux(std::ostream & os) const override;
+    virtual std::unique_ptr<element> clone() const override;
 private:
     int _niveau;
 };
 
 
+//les fantomes
+class fantome : public enmouvement{
+public:
+    fantome(position const & p);
+    taille tai() const override;
+    char typeobjet() const override{
+        return 'F';
+    };
+    virtual std::unique_ptr<element> clone() const override;
+    void changerdirection();
+};
+
+
+//les pacgommes
+class pacgomme : public element {
+public:
+    pacgomme(position const & p);
+    taille tai() const override;
+    char typeobjet() const override{
+        return 'G';
+    }
+    virtual std::unique_ptr<element> clone() const override;
+
+};
+
+
+//les murs
+class mur : public element{
+public:
+    mur(position const & p, taille const & t);
+    taille tai() const override;
+    char typeobjet() const override{
+        return 'M';
+    }
+    virtual std::unique_ptr<element> clone() const override;
+    static std::unique_ptr<mur> fabrique(position const & p, taille const & t);
+private:
+   taille _tai;
+};
 
 
 
+//la classe execption jeu
+class exceptionjeu : public std::exception{
+public:
+    exceptionjeu(std::string const & nature)
+        :std::exception(), _nature(nature){}
+
+    char const* what() const noexcept override;
+private:
+    std::string _nature;
+};
 
 
-
-
-
-
-
-
-
-
-
-
+//la classe jeu
+class jeu{
+public:
+    enum class type{
+        encours, defaite, victoire
+    };
+public:
+    jeu() : _etat(type::encours){};
+    jeu(jeu const & j);
+    jeu & operator=(jeu const & j);
+    void afficher(std::ostream & os) const;
+    void ajouter(std::unique_ptr<element> e);
+    void ajouterfantomes(int nbf);
+    void ajouterpacgommes(int nbg);
+    const pacman & accespacman() const;
+    void directionjoueur(direction dir);
+    void changerdirectionfantomes();
+    void tourdejeu();
+    type etatjeu(){
+        return _etat;
+    }
+    std::list<std::unique_ptr<element>> const & elements() const{
+        return _elements;
+    }
+private:
+    void appliquerdeplacementcollisionmur(enmouvement & em);
+    void appliquerdeplacementcontact(pacman & pm);
+    void appliquerdeplacementmanger(pacman & pm);
+private:
+    std::list<std::unique_ptr<element>> _elements;
+    type _etat;
+};
 
 
 
